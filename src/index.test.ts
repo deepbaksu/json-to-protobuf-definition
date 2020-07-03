82bff8e93d935cfcf787e462771a2611165040e8
Mo Kweon <kkweon@google.com>
2020-07-03 15:52:56 -0700

    fix: eslint errors after rebase

--- src/index.test.ts
+++ src/index.test.ts
@@ -6,27 +6,31 @@ import {
   ProtoPrimitive,
 } from './models/models_pb'
 
-it('should convert the basic object to ProtoMessage', () => {
-  const obj = {
-    keyName: 'mo kweon',
-  }
+describe('index', () => {
+  it('should convert the basic object to ProtoMessage', () => {
+    expect.hasAssertions()
 
-  // The expected proto
-  //
-  // message Root {
-  //   string keyName = 1;
-  // }
-  const expected = new ProtoMessage()
-  expected.setName('root')
+    const obj = {
+      keyName: 'mo kweon',
+    }
 
-  const protoField = new ProtoField()
-  protoField.setName('keyName')
-  protoField.setTag(1)
+    // The expected proto
+    //
+    // message Root {
+    //   string keyName = 1;
+    // }
+    const expected = new ProtoMessage()
+    expected.setName('root')
 
-  const protoType = new ProtoType()
-  protoType.setProtoTypePrimitive(ProtoPrimitive.PROTO_PRIMITIVE_STRING)
-  protoField.setType(protoType)
-  expected.addFields(protoField)
+    const protoField = new ProtoField()
+    protoField.setName('keyName')
+    protoField.setTag(1)
 
-  expect(parseRootObjectToProtoMessage(obj)).toEqual(expected)
+    const protoType = new ProtoType()
+    protoType.setProtoTypePrimitive(ProtoPrimitive.PROTO_PRIMITIVE_STRING)
+    protoField.setType(protoType)
+    expected.addFields(protoField)
+
+    expect(parseRootObjectToProtoMessage(obj)).toStrictEqual(expected)
+  })
 })
