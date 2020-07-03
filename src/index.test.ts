e6e39683fed68029fde9bc16f305b0d66e607aeb
Mo Kweon <kkweon@google.com>
2020-07-03 14:06:36 -0700

    test: add basic test cases

---
+++ src/index.test.ts
@@ -0,0 +1,32 @@
+import { parseRootObjectToProtoMessage } from '.'
+import {
+  ProtoMessage,
+  ProtoField,
+  ProtoType,
+  ProtoPrimitive,
+} from './models/models_pb'
+
+it('should convert the basic object to ProtoMessage', () => {
+  const obj = {
+    keyName: 'mo kweon',
+  }
+
+  // The expected proto
+  //
+  // message Root {
+  //   string keyName = 1;
+  // }
+  const expected = new ProtoMessage()
+  expected.setName('root')
+
+  const protoField = new ProtoField()
+  protoField.setName('keyName')
+  protoField.setTag(1)
+
+  const protoType = new ProtoType()
+  protoType.setProtoTypePrimitive(ProtoPrimitive.PROTO_PRIMITIVE_STRING)
+  protoField.setType(protoType)
+  expected.addFields(protoField)
+
+  expect(parseRootObjectToProtoMessage(obj)).toEqual(expected)
+})
