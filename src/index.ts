5f1205be62f9f41d46382dde6ac9f0978b594929
Mo Kweon <kkweon@google.com>
2020-07-03 10:08:46 -0700

    fix: run Prettier and add to the scripts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 export function generateProtoDefinition(json: string): string {
-  const parsedObject = JSON.parse(json);
+  const parsedObject = JSON.parse(json)
 
-  let i = 1;
+  let i = 1
 
   const messages = [
-    "message Root {",
+    'message Root {',
 
-    ...Object.keys(parsedObject).map(key => `any ${key} = ${i++}`),
+    ...Object.keys(parsedObject).map((key) => `any ${key} = ${i++}`),
 
-    "}"
-  ];
+    '}',
+  ]
 
-  return messages.join("\n");
+  return messages.join('\n')
 }
