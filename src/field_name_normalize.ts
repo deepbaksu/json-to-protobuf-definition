348fddd2440a1c81b2cbac710fb32c9ccb82648c
Mo Kweon <kkweon@google.com>
2020-07-03 21:22:45 -0700

    feat: add object and null type supports

--- src/field_name_normalize.ts
+++ src/field_name_normalize.ts
@@ -3,3 +3,4 @@ import snakeCase from 'lodash.snakecase'
 export function normalizeProtoFieldName(fieldName: string): string {
   return snakeCase(fieldName)
 }
+
--- src/parse_proto_field.ts
+++ src/parse_proto_field.ts
@@ -1,5 +1,11 @@
-import { ProtoField, ProtoPrimitive, ProtoType } from './models/models_pb'
+import {
+  ProtoField,
+  ProtoMessage,
+  ProtoPrimitive,
+  ProtoType,
+} from './models/models_pb'
 import { normalizeProtoFieldName } from './field_name_normalize'
+import pascalcase from 'pascalcase'
 
 // If there is no remainder when x % 1, it's an integer.
 function isInt(x: number): boolean {
@@ -39,6 +45,38 @@ export function parseProtoField(
       )
       protoField.setType(protoType)
       break
+    }
+
+    case 'object': {
+      // There are 3 possible case: null, array, object
+      // null and object are treated the same (creating an message).
+      // array will be repeated message
+
+      if (obj === null) {
+        const protoType = new ProtoType()
+        const protoMessage = new ProtoMessage()
+        protoMessage.setName(pascalcase(fieldName))
+        protoType.setProtoTypeMessage(protoMessage)
+
+        protoField.setType(protoType)
+        break
+      } else if (Array.isArray(obj)) {
+        // TODO(kkweon): Handle array
+      } else {
+        const protoMessage = new ProtoMessage()
+        protoMessage.setName(pascalcase(fieldName))
+
+        let tag = 1
+        // object
+        for (const [key, msg] of Object.entries(obj)) {
+          protoMessage.addFields(parseProtoField(msg, key, tag++))
+        }
+
+        const protoType = new ProtoType()
+        protoType.setProtoTypeMessage(protoMessage)
+
+        protoField.setType(protoType)
+      }
     }
   }
 
