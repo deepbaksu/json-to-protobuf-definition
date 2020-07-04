2696f7f15efef460bfc8c0712c13d3874152bb63
Mo Kweon <kkweon@google.com>
2020-07-04 14:57:45 -0700

    doc: add comments why the normalize is necessary

--- src/normalize_proto_field_name.ts
+++ src/normalize_proto_field_name.ts
@@ -1,5 +1,15 @@
 import snakeCase from 'lodash.snakecase'
 
+/**
+ * Normalize the field name as it should be snake_case for proto.
+ *
+ * message Root {
+ *   FieldType field_name = 1;
+ * }
+ *
+ * @param fieldName name to be snake cased
+ * @return snake cased name
+ */
 export function normalizeProtoFieldName(fieldName: string): string {
   return snakeCase(fieldName)
 }
