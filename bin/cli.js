8b66c1ebf9df6ea4b4e5ee9c7086321fd613ba89
Mo Kweon <kkweon@google.com>
2021-06-25 12:23:09 -0700

    feat: add CLI

---
+++ bin/cli.js
@@ -0,0 +1,9 @@
+const fs = require('fs')
+const lib = require('../dist/index')
+const data = fs.readFileSync(0, 'utf-8')
+
+console.log(
+  lib.convertProtoMessageToString(
+    lib.parseRootObjectToProtoMessage(JSON.parse(data)),
+  ),
+)
